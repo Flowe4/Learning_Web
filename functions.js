e6553dd45dfd6ea86e6e0ca5247c8e33fbdcd563
Flowe4 <eyowsporwsg@gmail.com>
2023-11-10 22:19:06 +0200

    got everything in center
    I got the label text box and submit button in middle and centered

--- functions.js
+++ functions.js
@@ -16,4 +16,5 @@ document.getElementById("Button").onclick = function() {
     user = document.getElementById("Text").value;
     console.log(user)
     document.getElementById("Label").innerHTML = "Hello " + user;
+    check
 }
