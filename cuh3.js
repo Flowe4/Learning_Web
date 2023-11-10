9b29799260f2c6cdebbe13f9d4d6a3da1dd30e75
Flowe4 <eyowsporwsg@gmail.com>
2023-11-10 12:44:41 +0200

    Initial commit

---
+++ cuh3.js
@@ -0,0 +1,19 @@
+console.log("fuck you get out of inspect");
+
+let name = "fuck my dic"
+let age = 21
+
+name = name + 4
+
+
+console.log(name)
+console.log(age)
+
+
+let user;
+
+document.getElementById("Button").onclick = function() {
+    user = document.getElementById("Text").value;
+    console.log(user)
+    document.getElementById("Label").innerHTML = "Hello " + user;
+}
