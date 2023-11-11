c11f3e56f7ab048578404d08126c23c097b4d3c4
Flowe4 <eyowsporwsg@gmail.com>
2023-11-11 23:10:56 +0200

    Changes again
    made the website 100000000000x better

--- functions.js
+++ functions.js
@@ -16,5 +16,4 @@ document.getElementById("Button").onclick = function() {
     user = document.getElementById("Text").value;
     console.log(user)
     document.getElementById("Label").innerHTML = "Hello " + user;
-    check
 }
